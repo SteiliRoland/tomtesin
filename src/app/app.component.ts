db49c37225c90304c8868022035e7af526442897
Steili Roland <steiliroland2001@gmail.com>
2022-01-13 10:01:41 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+/*
+* File: app.component.ts
+* Author: Steili Roland
+* Copyright: 2022,Steili Roland
+* Group: Szoft II/N
+* Date: 2022-01-13
+* Github: https://github.com/SteiliRoland/tomtesin
+* Licenc: GNU GPL
+*/
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +16,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tomtesin';
+  tomeg:number;
+  magassag:number;
+  testtomegindex:number;
+  constructor(){
+  this.tomeg=0;
+  this.magassag=0;
+  this.testtomegindex=0;
+  }
+
+  onClickSzamit(){
+    this.testtomegindex= this.tomeg / Math.pow(this.magassag,2);
+    
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+/*
+* File: app.modudele.ts
+* Author: Steili Roland
+* Copyright: 2022,Steili Roland
+* Group: Szoft II/N
+* Date: 2022-01-13
+* Github: https://github.com/SteiliRoland/tomtesin
+* Licenc: GNU GPL
+*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
